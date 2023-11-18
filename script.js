42a57cda3d932098d77acd00ee49aa3d6173023d
Emanuel Sceppaquercia <emanuelsce94@gmail.com>
2023-11-17 23:10:31 -0300

    Base structure added

---
+++ script.js
@@ -0,0 +1,230 @@
+var roadSigns = [{ id: 0, name: "no avanzar", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R1.jpg" },
+{ id: 1, name: "contramano", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R2.jpg" },
+{ id: 2, name: "no circular (autos)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R3_1.jpg" },
+{ id: 3, name: "no circular (motos)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R3_2.jpg" },
+{ id: 4, name: "no circular (bicicletas)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R3_3.jpg" },
+{ id: 5, name: "no circular (camiones)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R3_4.jpg" },
+{ id: 6, name: "no circular (acoplados)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R3_5.jpg" },
+{ id: 7, name: "no circular (peatones)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R3_6.jpg" },
+{ id: 8, name: "no circular (tracción animal)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R3_7.jpg" },
+{ id: 9, name: "no circular (jinetes)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R3_8.jpg" },
+{ id: 10, name: "no circular (carro de mano)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R3_9.jpg" },
+{ id: 11, name: "no circular (tractores)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R3_10.jpg" },
+{ id: 12, name: "no circular (opcional)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R3_11.jpg" },
+{ id: 13, name: "no girar a la izquierda", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R4_a.jpg" },
+{ id: 14, name: "no girar a la derecha", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R4_b.jpg" },
+{ id: 15, name: "no girar en 'u'", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R5.jpg" },
+{ id: 16, name: "no adelantar", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R6.jpg" },
+{ id: 17, name: "no ruidos molestos", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources_production/principal/cat_reglamentarias/reg_R7.jpg" },
+{ id: 18, name: "no estacionar", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R8.jpg" },
+{ id: 19, name: "no estacionar ni detenerse", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R9.jpg" },
+{ id: 20, name: "prohibición de cambiar de carril", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R10.jpg" },
+{ id: 21, name: "limitación de peso", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R11_a.jpg" },
+{ id: 22, name: "limitación de peso (por eje)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R11_b.jpg" },
+{ id: 23, name: "limitación de altura", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R12.jpg" },
+{ id: 24, name: "limitación de ancho", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R13.jpg" },
+{ id: 25, name: "limitación de largo del vehículo", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R14.jpg" },
+{ id: 26, name: "límite de velocidad máxima", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R15.jpg" },
+{ id: 27, name: "límite de velocidad mínima", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R16.jpg" },
+{ id: 28, name: "estacionamiento exclusivo", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/R17.jpg" },
+{ id: 29, name: "circulación exclusiva (transporte público)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R18_a.jpg" },
+{ id: 30, name: "circulación exclusiva (motos)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R18_b.jpg" },
+{ id: 31, name: "circulación exclusiva (bicicletas)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R18_c.jpg" },
+{ id: 32, name: "circulación exclusiva (jinetes)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R18_d.jpg" },
+{ id: 33, name: "circulación exclusiva (peatones)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R18_e.jpg" },
+{ id: 34, name: "uso de cadenas para nieve", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R19.jpg" },
+{ id: 35, name: "giro obligatorio derecha", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R20_a.jpg" },
+{ id: 36, name: "gito obligatorio izquierda", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R20_b.jpg" },
+{ id: 37, name: "sentido de circulación (derecha)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R21_a.jpg" },
+{ id: 38, name: "sentido de circulación (izquierda)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R21_b.jpg" },
+{ id: 39, name: "sentido de circulación (comienzo sentido único)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R21_c.jpg" },
+{ id: 40, name: "sentido de circulación (alternativa)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R21_d.jpg" },
+{ id: 41, name: "paso obligatorio (derecha)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R22_a.jpg" },
+{ id: 42, name: "paso obligatorio (izquierda)", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R22_b.jpg" },
+{ id: 43, name: "tránsito pesado a la derecha", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R23.jpg" },
+{ id: 44, name: "peatón a la izquierda", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R24.jpg" },
+{ id: 45, name: "puesto de control", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R25.jpg" },
+{ id: 46, name: "comienzo de doble mano", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R26.jpg" },
+{ id: 47, name: "pare", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R27.jpg" },
+{ id: 48, name: "ceda el paso", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R28.jpg" },
+{ id: 49, name: "preferencia de avance", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R29.jpg" },
+{ id: 50, name: "barreras ferroviales", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R30.jpg" },
+{ id: 51, name: "fin de la preescripción", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R31.jpg" },
+{ id: 52, name: "fin de la preescripción", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_reglamentarias/reg_R32.jpg" },
+{ id: 53, name: "luz baja obligatoria", type: "reglamentarias o prescriptivas", link: "http://www.senalar.com.ar/resources/cat_educativas/edu_07_EV.jpg" },
+
+{ id: 6, name: "cruce ferroviario", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P1.jpg" },
+{ id: 6, name: "paneles de prevención (objecto rígido)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P2_b.jpg" },
+{ id: 6, name: "paneles de prevención (curva/chevrón)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P2_c.jpg" },
+{ id: 6, name: "cruz de san andrés", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P3_c.jpg" },
+{ id: 6, name: "curva cerrada", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P4.jpg" },
+{ id: 6, name: "cruce de peatones", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P5.jpg" },
+{ id: 6, name: "atención", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P6.jpg" },
+{ id: 6, name: "curva (común y pronunciada)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P7_a.jpg" },
+{ id: 6, name: "curva (contracurva)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P7_b.jpg" },
+{ id: 6, name: "curva (en 's')", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P7_c.jpg" },
+{ id: 6, name: "camino sinuoso", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P8.jpg" },
+{ id: 6, name: "pendiente (descendente)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P9_a.jpg" },
+{ id: 6, name: "pendiente (ascendente)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P9_b.jpg" },
+{ id: 6, name: "estrechamiento (en una sola mano)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P10_b.jpg" },
+{ id: 6, name: "estrechamiento (en las dos manos)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P10_a.jpg" },
+{ id: 6, name: "perfil irregular (irregular)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P11_a.jpg" },
+{ id: 6, name: "perfil irregular (badén)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P11_b.jpg" },
+{ id: 6, name: "perfil irregular (lomada)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P11_c.jpg" },
+{ id: 6, name: "calzada resbaladiza", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P12.jpg" },
+{ id: 6, name: "proyección de piedras", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P13.jpg" },
+{ id: 6, name: "derrumbes", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P14.jpg" },
+{ id: 6, name: "túnel", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P15.jpg" },
+{ id: 6, name: "puente angosto", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P16.jpg" },
+{ id: 6, name: "puente móvil", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P17.jpg" },
+{ id: 6, name: "altura limitada", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P18.jpg" },
+{ id: 6, name: "ancho limitado", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P19.jpg" },
+{ id: 6, name: "principio y fin de calzada dividida", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P20.jpg" },
+{ id: 6, name: "rotonda", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P21.jpg" },
+{ id: 6, name: "incorporación de tránsito lateral", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P22.jpg" },
+{ id: 6, name: "inicio de doble circulación", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P23.jpg" },
+{ id: 6, name: "encrucijada (cruce)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P24_a.jpg" },
+{ id: 6, name: "encrucijada (empalme)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P24_b.jpg" },
+{ id: 6, name: "encrucijada (bifurcación)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P24_c.jpg" },
+{ id: 6, name: "encrucijada", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P24_d.jpg" },
+{ id: 6, name: "escolares", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P25_a.jpg" },
+{ id: 6, name: "niños", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P25_b.jpg" },
+{ id: 6, name: "ciclistas", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P26_a.jpg" },
+{ id: 6, name: "jinetes", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P26_b.jpg" },
+{ id: 6, name: "animales sueltos (vaca)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P27_a.jpg" },
+{ id: 6, name: "animales sueltos (ciervo)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P27_b.jpg" },
+{ id: 6, name: "corredor aéreo", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P28.jpg" },
+{ id: 6, name: "presencia de vehículos extraños (tranvía)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P29_a.jpg" },
+{ id: 6, name: "presencia de vehículos extraños (tractor)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P29_b.jpg" },
+{ id: 6, name: "presencia de vehículos extraños (ambulancia)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P29_c.jpg" },
+{ id: 6, name: "vientos fuertes laterales", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P30.jpg" },
+{ id: 6, name: "flecha direccional", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P31.jpg" },
+{ id: 6, name: "proximidad de semáforo", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P32.jpg" },
+{ id: 6, name: "proximidad de señal restrictiva (pare)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P33_a.jpg" },
+{ id: 6, name: "proximidad de señal restrictiva (paso)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P33_b.jpg" },
+{ id: 6, name: "proximidad de señal restrictiva (otras)", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P33_c.jpg" },
+{ id: 6, name: "fin de prevención", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P34.jpg" },
+{ id: 6, name: "paso a nivel sin barreras", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P40.jpg" },
+{ id: 6, name: "paso a nivel con barreras", type: "preventivas", link: "http://www.senalar.com.ar/resources/cat_preventivas/pre_P41.jpg" },
+
+{ id: 6, name: "nomenclatura urbana", type: "informativas", link: "http://www.senalar.com.ar/resources/cat_infotmativas/inf_4_a.jpg" },
+];
+
+var selectedSign;
+var selectedResponse = "";
+var score = 0;
+var round = 0;
+
+// This method select a random road sign that is different from the previous one
+function loadRandomRoadSign() {
+    var imgContainer = document.getElementById("imgContainer")
+    var randomIndex = getRandomIndex();
+
+    while (randomIndex === selectedSign?.id) {
+        randomIndex = getRandomIndex();
+    }
+
+    selectedSign = roadSigns[randomIndex];
+    imgContainer.src = selectedSign.link;
+}
+
+// This method return a list of new responses, including the right one
+function loadResponses() {
+    var responseList = document.getElementById("responseList");
+    var itemList = Array.from(responseList.children);
+
+    itemList[0].textContent = selectedSign.name;
+    itemList[1].textContent = getRandomRoadSignName(itemList);
+    itemList[2].textContent = getRandomRoadSignName(itemList);
+
+    // Elimina los elementos existentes de la lista
+    while (responseList.firstChild) {
+        responseList.removeChild(responseList.firstChild);
+    }
+
+    // Desordena la lista
+    itemList.sort(function () {
+        return Math.random() - 0.5;
+    });
+
+    // Vuelve a insertar los elementos desordenados en la lista
+    itemList.forEach(function (elemento) {
+        responseList.appendChild(elemento);
+    });
+}
+
+// This method returns a random name that is different from the selected road sign name
+function getRandomRoadSignName(itemList) {
+    var filteredRoadSigns = roadSigns.filter((sign) => sign.type === selectedSign.type);
+    var randomName = filteredRoadSigns[getRandomIndex(filteredRoadSigns)]?.name;
+
+    while (checkIfNameAlreadyExists(itemList, randomName)) {
+        randomName = roadSigns[getRandomIndex()]?.name;
+    }
+    return randomName;
+}
+
+function checkIfNameAlreadyExists(itemList, name) {
+    var nameIsPresent = false;
+
+    for (var i = 0; i < itemList.length; i++) {
+        if (itemList[i].textContent === name) {
+            nameIsPresent = true;
+            break;
+        }
+    }
+    return nameIsPresent;
+}
+
+// This method returns a random index from the road signs list
+function getRandomIndex(list) {
+    var listLenght = list ? list.length : roadSigns.length
+    return Math.floor(Math.random() * listLenght);
+}
+
+// Method triggered on an response item click
+function onItemClick(item) {
+    selectedResponse = item.textContent;
+
+    resetResponseStates()
+
+    item.style.backgroundColor = "#b7e2b0";
+}
+
+function resetResponseStates() {
+    var listItems = document.querySelectorAll('ul li');
+    listItems.forEach(function (item) {
+        // Remove the "highlight" class from all items
+        item.style.backgroundColor = "#ffffff";
+    });
+}
+
+function handleScore() {
+    if (selectedResponse === selectedSign.name) {
+        score++;
+    }
+    round++;
+
+    loadScores();
+}
+
+function loadScores() {
+    document.getElementById("score").textContent = score;
+    document.getElementById("round").textContent = round;
+}
+
+// Method triggered on the "next" button click
+function onNext() {
+    handleScore();
+    resetResponseStates();
+    loadRandomRoadSign();
+    loadResponses();
+}
+
+function onFirstRender() {
+    loadRandomRoadSign();
+    loadResponses();
+    loadScores();
+}
+
+onFirstRender();
